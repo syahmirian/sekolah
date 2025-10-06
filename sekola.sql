8c401be52f20d6403c336bf0db2362832234d647
Syahmi Rianta <syahmirianta.com>
2025-10-06 17:51:24 +0700

    Mengisi data siswa dan nilai

--- sekola.sql
+++ sekola.sql
@@ -11,3 +11,19 @@ CREATE TABLE nilai (
     mata_pelajaran VARCHAR(100),
     nilai INT
 );
+
+insert into siswa (nama, umur, jurusan)
+values 
+('Arimbi', 16, 'IPA'),
+('Dito', 17, 'IPS'),
+('Roro', 17, 'IPA'),
+('Bagas', 16, 'IPS'),
+('Jaki', 17, 'IPA');
+
+insert into nilai (siswa_id,mata_pelajaran,nilai)
+values 
+(1, 'Matematika', 98),
+(2, 'Bahasa Inggris', 88),
+(3, 'Bahasa Indonesia', 85),
+(4, 'Matematika', 82),
+(5, 'Bahasa Inggris', 91);
