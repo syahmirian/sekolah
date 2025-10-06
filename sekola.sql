4c746cb6a1522aa20e43114ffc4f74a66f1002bf
Syahmi Rianta <syahmirianta.com>
2025-10-06 17:56:14 +0700

    update siswa setelah terjadi penghapusan

--- sekola.sql
+++ sekola.sql
@@ -28,3 +28,31 @@ values
 (4, 'Matematika', 82),
 (5, 'Bahasa Inggris', 91);
 
+SELECT * FROM siswa;
+
+SELECT * FROM siswa WHERE jurusan = 'IPA';
+
+SELECT siswa.nama, AVG(nilai.nilai) AS rata_nilai
+FROM siswa
+JOIN nilai ON siswa.id = nilai.siswa_id
+GROUP BY siswa.nama
+ORDER BY siswa.nama;
+
+SELECT siswa.nama, siswa.jurusan, nilai.mata_pelajaran, nilai.nilai
+FROM siswa
+JOIN nilai ON siswa.id = nilai.siswa_id
+ORDER BY siswa.id;
+
+UPDATE siswa
+SET jurusan = 'IPS'
+WHERE nama = 'Arimbi';
+
+SELECT siswa.nama, siswa.jurusan, nilai.mata_pelajaran, nilai.nilai
+FROM siswa
+JOIN nilai ON siswa.id = nilai.siswa_id
+ORDER BY siswa.id;
+
+DELETE FROM nilai
+WHERE siswa_id = 4;
+
+SELECT * FROM nilai;
